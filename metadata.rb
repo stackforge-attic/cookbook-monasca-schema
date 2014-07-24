84181bb79d0851d34759a1ba68869920bd6ebc62
Tim Kuhlman <tim.kuhlman@hp.com>
2014-07-24 08:56:04 -0600

    Initial checkin

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+#
+name             'monasca_schema'
+maintainer       'Monasca Team'
+maintainer_email 'monasca@lists.launchpad.net'
+license          'All rights reserved'
+description      'Installs/Configures monasca_schema'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
