c996b5382d22459d322eeff86c88333c196fd9ae
Jonathan Halterman <jhalterman@gmail.com>
2014-09-15 16:23:52 -0700

    WIP - Initial support for alarmed metrics. Leaving sub_alarm tables for now.
    Change-Id: I5ccdb53ef4dadc3c8dbc68959de306f914236330

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ maintainer_email 'monasca@lists.launchpad.net'
 license          'All rights reserved'
 description      'Installs/Configures monasca_schema'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 depends          'influxdb'
