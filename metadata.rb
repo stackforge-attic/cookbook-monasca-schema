afa1f766786a99225cafc3b2bdb162ab46b22043
Craig Bryant <craig.bryant@hp.com>
2014-10-20 11:25:06 -0600

    Link sub_alarm to sub_alarm_expression
    Linking sub_alarm to sub_alarm_expression allows the Threshhold engine
    to change SubAlarms when the Alarm Definition is changed.
    
    Also, increase size of dimension_name and value in the
    sub_alarm_definition_dimension table to match what the API accepts
    
    Remove the primary key from the sub_alarm_definition_dimension
    table because increasing the size of dimension_name exceeds the
    size thet mysql will accept for an index. This key isn't really
    needed
    
    Bump the cookbook revision
    
    Change-Id: Ic2e84a24fe051eb46b98ed6ef57e28cccda80f53

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ maintainer_email 'monasca@lists.launchpad.net'
 license          'All rights reserved'
 description      'Installs/Configures monasca_schema'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 depends          'influxdb'
